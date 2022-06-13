9ea74b7491f4bea93913a9f411b283386319a66e
Diana <olhadianaaa@gmail.com>
2022-06-13 15:05:49 +0300

    simple program

---
+++ lesson5/main.go
@@ -0,0 +1,10 @@
+package main
+// may unclude a lot of files
+import(
+	"fmt"
+)
+func main() {
+	// that's logic of my program. From this point starts everything. And it will include everything. .
+	fmt.Print ("hhhhh")
+	fmt.Print("Glory to Heroes!!!")
+}
